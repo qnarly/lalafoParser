387d7e032cbd6745ba756cb153ffca2f1375e12f
tursunovAlihan <alihantursunov606@gmail.com>
2025-11-19 20:08:48 +0600

    create project

---
+++ src/main/java/kg/lalafoparser/LalafoParserApplication.java
@@ -0,0 +1,13 @@
+package kg.lalafoparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LalafoParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LalafoParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/lalafoparser/LalafoParserApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.lalafoparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LalafoParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
