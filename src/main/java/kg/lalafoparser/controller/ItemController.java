765bf84b20388444b21909c885e9690a73ae44fa
tursunovAlihan <alihantursunov606@gmail.com>
2025-11-19 21:46:59 +0600

    add ItemController and ItemService interface with getItems method

--- src/main/java/kg/lalafoparser/controller/ItemController.java
+++ src/main/java/kg/lalafoparser/controller/ItemController.java
@@ -1,4 +1,21 @@
 package kg.lalafoparser.controller;
 
+import kg.lalafoparser.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/test-result")
+    public String getTestResultPage(Model model) {
+        var its = itemService.getItems(100);
+        model.addAttribute("items", its);
+        return "test-result";
+    }
 }
--- src/main/java/kg/lalafoparser/service/ItemService.java
+++ src/main/java/kg/lalafoparser/service/ItemService.java
@@ -1,4 +1,9 @@
 package kg.lalafoparser.service;
 
+import kg.lalafoparser.dto.ItemDto;
+
+import java.util.List;
+
 public interface ItemService {
+    List<ItemDto> getItems(Integer limit);
 }
